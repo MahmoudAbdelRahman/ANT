74103a60b1b0f49ffaf237a28acc8b5ce32de8da
Mahmoud AbdelRahman <arch.mahmoud.ouf111@gmail.com>
2017-06-05 15:15:38 +0200

    Updated the main component description

--- ant/ant/antComponent.cs
+++ ant/ant/antComponent.cs
@@ -52,6 +52,27 @@ namespace ANT
                 "Start The ant Colony ... ",
                 "ANT", "0_ant")
         {
+            this.Description = "BSD 2-Clause License"+
+                "\n\nCopyright (c) 2017, Mahmoud AbdelRahman <arch.mahmoud.ouf111@gmail.com>"+
+                "\nhttp://www.m-ouf.com/"+
+                "\nAll rights reserved."+
+                "\n\nRedistribution and use in source and binary forms, with or without"+
+                "\nmodification, are permitted provided that the following conditions are met:"+
+                "\n* Redistributions of source code must retain the above copyright notice, this"+
+                "\n  list of conditions and the following disclaimer."+
+                "\n\n* Redistributions in binary form must reproduce the above copyright notice,"+
+                "\n  this list of conditions and the following disclaimer in the documentation"+
+                "\n  and/or other materials provided with the distribution."+
+                "\n\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\""+
+                "\nAND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE"+
+                "\nIMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE"+
+                "\nDISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE"+
+                "\nFOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL"+
+                "\nDAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR"+
+                "\nSERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER"+
+                "\nCAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,"+
+                "\nOR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE"+
+                "\nOF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
             if(!System.IO.Directory.Exists(MainDirectory+MainFolder))
             {
                 System.IO.Directory.CreateDirectory(MainDirectory+MainFolder);
